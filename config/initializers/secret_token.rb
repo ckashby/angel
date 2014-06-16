895a4da94bf1e95ec408581f8730d4893c7d4021
Cap Kello <captainkello@gmail.com>
2014-06-16 14:00:23 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angel::Application.config.secret_token = '541d54387ba9e5ce66c1b40d75229a3b4491031658d47b41ccf39672d65300c6f3341c317279c0b5cdb6e1d14a3309f3d032f36a80f0a974403de206e5699f74'
